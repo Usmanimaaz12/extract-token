a2794ff90661e5d1327afd91e696a57c68a996b0
Maaz Usmani <maaz.usmani@squareboat.com>
2024-07-26 11:41:52 +0530

    ADD : redirect URI

--- shopifyOAuthHelper.js
+++ shopifyOAuthHelper.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const authorize = async (shop) => {
   return encodeURI(
-    `https://${shop}.myshopify.com/admin/oauth/authorize?client_id=${process.env.client_id}&scope=${process.env.scopes}&redirect_uri=${process.env.redirect_uri}`
+    `https://${shop}.myshopify.com/admin/oauth/authorize?client_id=${process.env.client_id}&scope=${process.env.scopes}&redirect_uri="https://extract-token.onrender.com/api/shopify/redirect"`
   );
 };
 
